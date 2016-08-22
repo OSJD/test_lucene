351726e41b49334eee4a10b5c4b24337a7a1bb38
Janaka Chathuranga <bjchathuranga@gmail.com>
2016-08-22 11:43:55 +0530

    Added LatLon and Geo Queries

--- src/main/java/LatLonSample.java
+++ src/main/java/LatLonSample.java
@@ -1,6 +1,7 @@
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.*;
+import org.apache.lucene.geo.Polygon;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
@@ -10,7 +11,12 @@ import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
+import org.apache.lucene.spatial3d.Geo3DPoint;
+
+
+
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Paths;
@@ -22,6 +28,8 @@ public class LatLonSample {
 
 
     private IndexSearcher searcher;
+    private double[] mapLat;
+    private double[] mapLon;
     private void generateTheIndex() throws IOException {
         Directory dir = FSDirectory.open(Paths.get("./index_airplane"));
         Analyzer analyzer = new StandardAnalyzer();
@@ -56,6 +64,9 @@ public class LatLonSample {
                 //Add a latlon point to index
                 try {
                     doc.add(new LatLonPoint("latlon", Double.parseDouble(tags[6]), Double.parseDouble(tags[7])));
+                    Geo3DPoint point = new Geo3DPoint("geo3d",Double.parseDouble(tags[6]), Double.parseDouble(tags[7]));
+                    doc.add(point);
+
                 }
                 catch (Exception e)
                 {
@@ -70,9 +81,35 @@ public class LatLonSample {
     }
 
     public void search() throws IOException {
-        System.out.println("\nLatLonQuery --------------------------------------");
+        System.out.println("\nLatLonQuery  Around Colombo 500km radius --------------------------------------");
 
-        TopDocs docs = searcher.search(LatLonPoint.newDistanceQuery("latlon",  6.9270790, 79.8612430, 500*1000  ),10);
+        TopDocs docs = searcher.search(LatLonPoint.newDistanceQuery("latlon",  6.9270790, 79.8612430, 500*1000  ),20);
+        for (ScoreDoc scoreDoc: docs.scoreDocs
+                ) {
+            Document doc = searcher.doc(scoreDoc.doc);
+
+            System.out.println(doc);
+
+        }
+
+        System.out.println("-----------------------------------------------------------------");
+
+        System.out.println("\nLatLonQuery  Polygon Query - Sri Lanka-------------------------------------");
+
+        docs = searcher.search(LatLonPoint.newPolygonQuery("latlon",new Polygon(mapLat,mapLon)),20);
+        for (ScoreDoc scoreDoc: docs.scoreDocs
+                ) {
+            Document doc = searcher.doc(scoreDoc.doc);
+
+            System.out.println(doc);
+
+        }
+
+        System.out.println("-----------------------------------------------------------------");
+
+        System.out.println("\nLatLonQuery  Geo3D Query - Sri Lanka-------------------------------------");
+
+        docs = searcher.search(Geo3DPoint.newPolygonQuery("geo3d",new Polygon(mapLat,mapLon)),20);
         for (ScoreDoc scoreDoc: docs.scoreDocs
                 ) {
             Document doc = searcher.doc(scoreDoc.doc);
@@ -85,9 +122,35 @@ public class LatLonSample {
 
     }
 
+    public void loadMap() throws FileNotFoundException {
+        try (BufferedReader br = new BufferedReader(new FileReader("./data/Sri_Lanka"))) {
+            String line = br.readLine();
+            String[] rows = line.split(" ");
+            mapLat =  new double[rows.length];
+            mapLon = new double[rows.length];
+
+            int count = 0;
+            for (String row:rows
+                 ) {
+                String[] splitedRow = row.split(",");
+                mapLat[count] = Double.parseDouble(splitedRow[1]);
+                mapLon[count] = Double.parseDouble(splitedRow[0]);
+
+                count++;
+            }
+
+
+        }
+        catch (Exception e)
+        {
+
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         LatLonSample sample  = new LatLonSample();
         sample.generateTheIndex();
+        sample.loadMap();
         sample.search();
     }
 
